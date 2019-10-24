301f54eb49036d8637f37c15bb9831ae9564a8fa
Ben Whitfield <ben.whitfield@ansgrp-002884.home>
2019-10-24 17:10:57 +0100

    add pagiation and count

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,29 @@
-const feed = (parent, args, context, info) => context.prisma.links();
+const feed = async (parent, args, context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count();
+
+  return { links, count };
+};
 
 const link = (parent, args, context, info) => {
   context.prisma.links().filter(link => link.id === args.id)[0];
